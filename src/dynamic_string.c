8b60623c6c782c7435d8d30c65f97e54b9935b15
Simon Sedlacek <saimon.sedlacek@gmail.com>
2019-10-24 17:56:47 +0200

    Added some operations for string_t manipulation, corrected some errors, refactor

--- src/dynamic_string.c
+++ src/dynamic_string.c
@@ -1,56 +1,105 @@
-//
-// Created by root on 10/17/19.
-//
+/**
+ * @file dynamic_string.c
+ * @brief This module implements all the necessary operations needed for manipulating
+ * our dynamic string implementation.
+ * @author xosval03, xsedla1h
+ */
 
 #include "dynamic_string.h"
 
+string_t *string_create_init() {
+    string_t *s = malloc(sizeof(string_t));
+    if (s == NULL) {
+        fprintf(stderr, "String ERROR: Could not allocate memory for a string.\n");
+        return NULL;
+    }
 
-void string_init(struct tString *s) {
-    s->index = 0;
-    s->real = 100;
-    s->array = calloc(s->real, 1);
-    if (s->array == NULL) {
-        fprintf(stderr, "Allocation problem");
-        exit(1);
+    if (string_init(s) == STR_ALLOC_ERROR) {
+        string_free(s);
+        return NULL;
     }
 }
 
-
-void string_append(struct tString *s, char c) {
-
+int string_init(string_t *s) {
+    s->index = 0;
+    s->real = STR_DEFAULT_LEN;
+    s->array = calloc(STR_DEFAULT_LEN, 1);
     if (s->array == NULL) {
-        fprintf(stderr, "Parameter NULL");
-        exit(1);
+        fprintf(stderr, "String ERROR: Could not init string.\n");
+        return STR_ALLOC_ERROR;
     }
+    return SUCCESS;
+}
 
+
+int string_append(string_t *s, char c) {
+    if (s == NULL) {
+        fprintf(stderr, "String WARNING: A pointer passed to string_append was NULL.\n");
+        return STR_NULL_PASSED;
+    }
 
     if (s->index + 2 >= s->real) {
         s->real = s->real * 2;
-        s->array = realloc(s->array, s->real);
-        if (s->array == NULL) {
-            fprintf(stderr, "Allocation problem");
-            exit(1);
+        if (!realloc(s->array, s->real))
+            fprintf(stderr, "String ERROR: Could not append char to string.\n");
+            return STR_ALLOC_ERROR;
         }
-
     }
-    s->array[s->index++] = c;
-
+    sprintf(&(s->array[s->index++]), "%c\0", c);
+    return SUCCESS;
 }
 
 
-void string_free(struct tString *s) {
 
-    if (s->array == NULL) {
-        fprintf(stderr, "Parameter NULL");
-        exit(1);
+void string_print(string_t *s, const char *append) {
+    if (s == NULL) {
+        fprintf(stderr, "String WARNING: A pointer passed to string_print was NULL.\n");
+        return;
     }
-
-    free(s->array);
+    printf("%s%s", s->array, append);
 }
 
-
-void string_print(struct tString *s) {
-    for (int i = 0; i < s->index; i++) {
-        printf("%c", s->array[i]);
+void string_free(string_t *s) {
+    if (s) {
+        free(s->array);
+        free(s);
+    } else {
+        fprintf(stderr, "String WARNING: A pointer passed to string_free was NULL.\n");
     }
+}
+
+void string_free_array(string_t *s) {
+    if (s) {
+        free(s->array);
+        s->real = 0;
+        s->index = 0;
+    } else {
+        fprintf(stderr, "String WARNING: A pointer passed to\
+                string_free_array was NULL.\n");
+    }
+}
+
+void string_clear(string_t *s) {
+    if (s) {
+        s->array[0] = '\0';
+        s->index = 0;
+    } else {
+        fprintf(stderr, "String WARNING: A pointer passed to string_clear was NULL.\n");
+    }
+}
+
+char *string_copy_data(string_t *s) {
+    if (s == NULL) {
+        fprintf(stderr, "String WARNING: A pointer passed to string_clear was NULL.\n");
+        return NULL;
+    }
+
+    char *new = calloc(s->index + 1, 1);
+    if (new == NULL) {
+        fprintf(stderr, "String ERROR: Could not allocate memory for\
+                the copy of the string.\n");
+        return NULL;
+    }
+    strcpy(new, s->array);
+    return new;
 }
--- src/dynamic_string.h
+++ src/dynamic_string.h
@@ -1,31 +1,77 @@
-//
-// Created by root on 10/17/19.
-//
+/**
+ * @file dynamic_string.h
+ * @brief A header file for dynamic_string.c
+ * @author xosval03, xsedla1h
+ */
 
-#ifndef UNTITLED_DYNAMIC_STRING_H
-#define UNTITLED_DYNAMIC_STRING_H
+#ifndef DYNAMIC_STRING_H
+#define DYNAMIC_STRING_H
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 
-struct tString {
+/* Default string length for initialization */
+#define STR_DEFAULT_LEN 100
+
+/* Return codes */
+#define SUCCESS 0
+#define STR_ALLOC_ERROR 1
+#define STR_NULL_PASSED 2
+
+/**
+ * @brief This structure represents our dynamic string.
+ */
+typedef struct String {
     char *array;
     int index;
     int real;
-} t_string;
+} string_t;
 
 
-void string_init(struct tString *s);
+/**
+ * @brief Allocates memory for a new string and initializes it.
+ * @return Pointer to the new string or NULL if the operation fails.
+ */
+string_t *string_create_init();
 
-void string_append(struct tString *s, char c);
+/**
+ * @brief Initializes s to the base values for a new string.
+ */
+int string_init(string_t *s);
 
-void string_print(struct tString *s);
+/**
+ * @brief Appends a character to s.
+ */
+int string_append(string_t *s, char c);
 
-void string_free(struct tString *s);
+/**
+ * @brief Prints out s->array.
+ */
+void string_print(string_t *s, const char *append);
 
+/**
+ * @brief Frees all the memory allocated for s->array and for s itself.
+ */
+void string_free(string_t *s);
 
+/**
+ * @brief Frees the memory allocated for s->array, sets s->index and s->real to zero.
+ */
+void string_free_array(string_t *s);
 
+/**
+ * @brief Reinits the string - clears the character array, sets index to 0.
+ */
+void string_clear(string_t *s);
 
+/**
+ * @brief Allocates memory for a new array of chars, then copies
+ * the content of s->array into the new char array.
+ * @return Returns a pointer to the new char array or NULL if the operation could
+ * not be completed.
+ */
+char *string_copy_data(string_t *s);
 
-
-#endif //UNTITLED_DYNAMIC_STRING_H
+#endif
