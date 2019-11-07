45f0f0d2fc5f3c4a1a4ea76ab73445c9c0a6cfd4
Simon Sedlacek <saimon.sedlacek@gmail.com>
2019-11-07 17:27:33 +0100

    Added some basic structures representing symbols in the symbol table, still experimenting

--- src/symtable.c
+++ src/symtable.c
@@ -6,28 +6,22 @@
  *	@note	TODO: revidovat funkce, struktury
  */
 
-#include "htab.h"
+#include "symtable.h"
 
 
-bool htab_iterator_valid(htab_iterator_t it) {
+bool symtable_iterator_valid(symtable_iterator_t it) {
     return it.ptr!=NULL;
 }
 
-bool htab_iterator_equal(htab_iterator_t it1, htab_iterator_t it2) {
+bool symtable_iterator_equal(symtable_iterator_t it1, symtable_iterator_t it2) {
     return it1.ptr==it2.ptr && it1.t == it2.t;
 }
 
-size_t htab_bucket_count(const htab_t *t) {
-    if (t == NULL)
-        return 0;
-    return t->arr_size;
-}
-
-void htab_clear(htab_t *t) {
+void symtable_clear(symtable_t *t) {
     if (t == NULL)
         return;
 
-    struct htab_item *next;
+    symbol_t *next;
 
     for (size_t i = 0; i < t->arr_size; i++) {
 
@@ -44,12 +38,12 @@ void htab_clear(htab_t *t) {
     t->size = 0;
 }
 
-void htab_free(htab_t *t) {
-    htab_clear(t);
+void symtable_free(symtable_t *t) {
+    symtable_clear(t);
     free(t);
 }
 
-unsigned int htab_hash_function(const char *str) {
+unsigned int symtable_hash_function(const char *str) {
     uint32_t h = 0;
     const unsigned char *p;
 
@@ -59,31 +53,17 @@ unsigned int htab_hash_function(const char *str) {
     return h;
 }
 
-htab_t *htab_init(size_t n) {
-    if (n == 0) {
-        fprintf(stderr, "Error: tabulka musi mit alespon jeden radek\n");
-        return NULL;
+void symtable_init(symtable_t *t) {
+    t->size = 0;
+    t->arr_size = SYMTABLE_SIZE;
+
+    for (size_t i = 0; i < SYMTABLE_SIZE; i++) {
+        t->array[i] = NULL;
     }
-
-    htab_t *new = malloc(sizeof(htab_t) + sizeof(struct htab_item *) * n);
-
-    if (new == NULL) {
-        fprintf(stderr, "Error: nepodarilo se alokovat pamet pro tabulku\n");
-        return NULL;
-    }
-
-    new->size = 0;
-    new->arr_size = n;
-
-    for (size_t i = 0; i < n; i++) {
-        new->array[i] = NULL;
-    }
-
-    return new;
 }
 
-htab_iterator_t htab_begin(const htab_t *t) {
-    htab_iterator_t begin;
+symtable_iterator_t symtable_begin(const symtable_t *t) {
+    symtable_iterator_t begin;
     begin.ptr = NULL;
     begin.t = NULL;
     begin.idx = 0;
@@ -104,9 +84,9 @@ htab_iterator_t htab_begin(const htab_t *t) {
     return begin;
 }
 
-htab_iterator_t htab_end(const htab_t *t) {
+symtable_iterator_t symtable_end(const symtable_t *t) {
 
-    htab_iterator_t end;
+    symtable_iterator_t end;
     end.ptr = NULL;
 
     if (t == NULL) {
@@ -125,13 +105,13 @@ htab_iterator_t htab_end(const htab_t *t) {
     return end;
 }
 
-const char *htab_iterator_get_key(htab_iterator_t it) {
+const char *symtable_iterator_get_key(symtable_iterator_t it) {
     assert(it.ptr != NULL);
     return it.ptr->key;
 }
 
-htab_iterator_t htab_iterator_next(htab_iterator_t it) {
-    if (!htab_iterator_valid(it))
+symtable_iterator_t symtable_iterator_next(symtable_iterator_t it) {
+    if (!symtable_iterator_valid(it))
         return it;
 
     /* V pripade, ze aktualni zaznam je v seznamu posledni */
@@ -148,7 +128,7 @@ htab_iterator_t htab_iterator_next(htab_iterator_t it) {
                 }
             }
 
-        it = htab_end(it.t);
+        it = symtable_end(it.t);
 
     } else {
         it.ptr = it.ptr->next;
@@ -157,97 +137,60 @@ htab_iterator_t htab_iterator_next(htab_iterator_t it) {
     return it;
 }
 
-int htab_iterator_get_value(htab_iterator_t it) {
+int symtable_iterator_get_value(symtable_iterator_t it) {
     assert(it.ptr != NULL);
     return it.ptr->data;
 }
 
-int htab_iterator_set_value(htab_iterator_t it, int val) {
+int symtable_iterator_set_value(symtable_iterator_t it, int val) {
     assert(it.ptr != NULL);
     return it.ptr->data = val;
 }
 
-htab_iterator_t htab_lookup_add(htab_t *t, const char *key) {
+symtable_iterator_t symtable_lookup_add(symtable_t *t, const char *key) {
     if (t == NULL || key == NULL) {
         fprintf(stderr, "Error: tabulka nebo klic neexistuji\n");
-        return htab_end(t);
+        return symtable_end(t);
     }
 
-    int index = (htab_hash_function(key) % t->arr_size);
-    htab_iterator_t it = { .ptr = t->array[index], .t = t, .idx = index };
+    int index = (symtable_hash_function(key) % t->arr_size);
+    symtable_iterator_t it = { .ptr = t->array[index], .t = t, .idx = index };
 
     /* Projdeme cely seznam na danem indexu a zjistime, zda se v tabulce jiz
      * dane slovo nachazi */
-    while (htab_iterator_valid(it) && it.idx == index) {
-        if (!strcmp(htab_iterator_get_key(it), key)) {
+    while (symtable_iterator_valid(it) && it.idx == index) {
+        if (!strcmp(symtable_iterator_get_key(it), key)) {
             return it;
         }
 
-        it = htab_iterator_next(it);
+        it = symtable_iterator_next(it);
     }
 
     /* Tabulka danou polozku neobsahovala, tudiz musime polozku pridat */
-    it.ptr = malloc(sizeof(struct htab_item));
+    it.ptr = malloc(sizeof(symbol__t));
     if (it.ptr == NULL) {
         fprintf(stderr, "Error: nepodarilo se vlozit klic do tabulky\n");
-        return htab_end(t);
+        return symtable_end(t);
     }
 
     /* Alokace a kontrola alokace pameti pro novy zaznam */
     it.ptr->key = malloc(strlen(key) + 1);
 
-    if (htab_iterator_get_key(it) == NULL) {
+    if (symtable_iterator_get_key(it) == NULL) {
         free(it.ptr);
-        return htab_end(t);
+        return symtable_end(t);
     }
 
     /* Inicializace nove polozky a uprava dat tabulky */
     strcpy(it.ptr->key, key);
-    htab_iterator_set_value(it, 0);
+    symtable_iterator_set_value(it, 0);
     it.ptr->next = t->array[index];
     t->array[index] = it.ptr;
     t->size++;
     return it;
 }
 
-htab_t *htab_move(size_t n, htab_t *from) {
-    if (from == NULL || n == 0)
-        return NULL;
-
-    htab_t *new = htab_init(n);
-
-    if (new == NULL) {
-        fprintf(stderr, "Error: nepodarilo se alokovat pamet "
-                "pro novou tabulku\n");
-        return NULL;
-    }
-
-    /* Pomocne iteratory pro prochazeni puvodni tabulkou a
-     * kontrolu operace vlozeni zaznamu do nove tabulky */
-    htab_iterator_t iterator, check;
-
-    /* Vsechny polozky z puvodni tabulky postupne vlozime do nove tabulky */
-    for (iterator = htab_begin(from);
-            htab_iterator_valid(iterator);
-            iterator = htab_iterator_next(iterator)) {
-
-        check = htab_lookup_add(new, iterator.ptr->key);
-        
-        if (!htab_iterator_valid(check)) {
-            fprintf(stderr, "Error: nepodarilo se presunout polozku "
-                    "do nove tabulky\n");
-            htab_clear(new);
-            return NULL;
-        }
-
-        htab_iterator_set_value(check, htab_iterator_get_value(iterator));
-    }
-
-    htab_clear(from);
-    return new;
-}
-
-size_t htab_size(const htab_t *t) {
+size_t symtable_size(const symtable_t *t) {
     if (t == NULL)
         return 0;
     return t->size;
---
+++ src/symtable.h
@@ -0,0 +1,164 @@
+/**
+ *	@file   symtable.h
+ *	@author	Simon Sedlacek, xsedla1h
+ *	@date	
+ *	@brief  Hlavickovy soubor pro symtable.c
+ *	@note	TODO: revidovat funkce, struktury
+ */
+
+#ifndef __SYMTABLE_H__
+#define __SYMTABLE_H__
+
+#include <string.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "scanner.h"
+
+#define SYMTABLE_SIZE 1233
+
+/**
+ * @brief Reprezentuje typ symbolu.
+ */
+typedef enum {
+    STYPE_VAR,
+    STYPE_FUNC,
+    STYPE_LABEL
+} symbol_type_t;
+
+/**
+ * @brief Reprezentuje typ promenne.
+ */
+typedef enum {
+    VTYPE_INT,
+    VTYPE_DBL,
+    VTYPE_STR
+} var_type_t;
+
+
+/**
+ * @struct variable_attributes
+ * @brief Struktura reprezentujici atributy promenne
+ *
+ * @var defined Indikuje, zda byla promenna jiz definovana
+ * @var type Typ promenne
+ * @var scope Reprezentuje scope promenne
+ * @var int_value Int hodnota promenne
+ * @var double_value Double hodnota promenne
+ * @var string_value String hodnota promenne
+ */
+typedef struct variable_attributes {
+    bool defined;
+    var_type_t type;
+    int scope; // nejsem si jisty, jak to zatim reprezentovat TODO
+    union {
+        int int_value;
+        double double_value;
+        char *string_value;
+    };
+} var_att_t;
+
+/**
+ * @struct function_attributes
+ * @brief Struktura reprezentujici atributy funkce
+ *
+ * @var defined Indikuje, zda byla funkce jiz definovana
+ * @var param_count Pocet parametru deklarovany pri definici funkce
+ */
+typedef struct function_attributes {
+    bool defined;
+    int param_count;
+} func_att_t;
+
+/**
+ * @struct label_attributes
+ * @brief Struktura reprezentujici atributy navesti
+ *
+ * @var defined Indikuje, zda bylo navesti jiz definovane
+ */
+typedef struct label_attributes {
+    bool defined;
+} label_att_t;
+
+/**
+ * @union symbol_attributes
+ * @brief Unie uchovavajici atributy promenne
+ *
+ * @var var_att Atributy promenne
+ * @var func_att Atributy funkce
+ * @var label_att Atributy navesti
+ */
+typedef union {
+    var_att_t var_att;
+    func_att_t func_att;
+    label_att_t label_att;
+} symbol_attributes;
+/**
+ * @struct symbol
+ * @brief Struktura reprezentujici jeden symbol
+ *
+ * @var id   Identifikator symbolu
+ * @var type Typ symbolu
+ * @var attributes Unie uchovavajici informace o symbolu potrebne pro semantickou analyzu
+ * @var data Pocet zaznamu vazajicich se k danemu klici.
+ * @var next Ukazatel na dalsi polozku seznamu.
+ */
+struct symbol {
+    char *id;
+    symbol_type_t type;
+    symbol_attributes attributes;
+    int data;
+    struct symbol *next;
+} symbol_t;
+
+/**
+ * @struct symtable
+ * @brief Struktura hashovaci tabulky.
+ *
+ * @var size     Aktualni pocet zaznamu v tabulce.
+ * @var arr_size Pocet radku tabulky.
+ * @var array    Pole seznamu se zaznamy.
+ */
+typedef struct symtable {
+    size_t size;
+    symbol_t *array[SYMTABLE_SIZE];
+} symtable_t;
+
+/**
+ * @struct symtable_iterator
+ * @brief Struktura iteratoru pro pruchod tabulkou
+ *
+ * @var ptr Ukazatel na aktualni polozku v tabulce.
+ * @var t   Ukazatel na prochazenou tabulku.
+ * @var idx Index urcujici, v kterem seznamu tabulky se polozka nachazi.
+ */
+typedef struct symtable_iterator {
+    symbol_t *ptr;
+    const symtable_t *t;
+    unsigned idx;
+} symtable_iterator_t;
+
+// rozptylovací (hash) funkce
+unsigned int symtable_hash_function(const char *str);
+
+// funkce pro práci s tabulkou:
+void symtable_init(symtable_t *t);
+void symtable_clear(symtable_t *t); // TODO
+void symtable_free(symtable_t *t); // TODO
+
+size_t symtable_size(const symtable_t *t); // počet záznamů v tabulce
+
+symtable_iterator_t symtable_lookup_add(symtable_t *t, const char *key); // TODO
+symtable_iterator_t symtable_begin(const symtable_t *t);
+symtable_iterator_t symtable_end(const symtable_t *t);
+symtable_iterator_t symtable_iterator_next(symtable_iterator_t it);
+
+bool symtable_iterator_valid(symtable_iterator_t it);
+bool symtable_iterator_equal(symtable_iterator_t it1, symtable_iterator_t it2);
+
+const char *symtable_iterator_get_key(symtable_iterator_t it);
+int symtable_iterator_get_value(symtable_iterator_t it);
+int symtable_iterator_set_value(symtable_iterator_t it, int val);
+
+#endif
