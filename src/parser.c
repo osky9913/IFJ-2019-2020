661345a2bf73784c213277873c070ae326301be8
Simon Sedlacek <saimon.sedlacek@gmail.com>
2019-11-15 18:18:19 +0100

    adding parser source files

---
+++ src/parser.c
@@ -0,0 +1,6 @@
+/**
+ *	@file   parser.c
+ *	@author Simon Sedlacek, xsedla1h
+ *	@date
+ *	@brief Parser prekladace.
+ */
---
+++ src/parser.h
@@ -0,0 +1,11 @@
+/**
+ *	@file   parser.h
+ *	@author Simon Sedlacek, xsedla1h
+ *	@date
+ *	@brief Hlavickovy soubor pro parser.
+ */
+
+#ifndef PARSER_H
+#define PARSER_H
+
+#endif
