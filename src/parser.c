57e3a37ae3d426fe85abfc351cdf34e28133f7a2
Simon Sedlacek <saimon.sedlacek@gmail.com>
2019-11-16 15:09:44 +0100

    adding placeholder functions for recursive dedent

--- src/parser.c
+++ src/parser.c
@@ -6,5 +6,31 @@
  */
 
 
-int recursive_dedent() {
+int r_program() {
+}
+int r_statement() {
+}
+int r_statement_list() {
+}
+int r_function_def() {
+}
+int r_param_list() {
+}
+int r_params() {
+}
+int r_if_else() {
+}
+int r_cycle() {
+}
+int r_function_ret() {
+}
+int r_retvalue() {
+}
+int r_value() {
+}
+int r_rest() {
+}
+int r_function_call() {
+}
+int r_term() {
 }
